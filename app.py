24504ec7d6bfda3089a337d20a00ac7230ac9433
Hariharan <hariharanbindu2019@gmail.com>
2024-07-31 10:46:33 +0530

    All the files are Modified.
    Dashboards are created respectively.

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/influencer_dashboard",methods=["GET","POST"])
+def influencer_dashboard():
+    data ={
+        "name":"Hariharan",
+        "username":"Hari",
+        "email":"hari@gmail.com",
+        "category":"Food",
+        "niche":"Street Food"
+    }
+    return render_template("influencer_dashboard.html",d= data)
+
+@app.route("/sponsor_dashboard",methods=["GET","POST"])
+def sponsor_dashboard():
+    data ={
+        "name":"Harry",
+        "username":"harry",
+        "email":"harry@gmail.com",
+        "company":"Tata",
+        "city":"Chennai"
+    }
+    return render_template("sponsor_dashboard.html",d= data)  
+
+'''@app.route("/profile_influencer",methods=["GET","POST"])
+def profile_influencer():
+    return render_template("influencer_dashboard.html",name="Hariharan")
+
+@app.route("/find_influencer",methods=["GET","POST"])
+def find_influencer():
+    return render_template("influencer_dashboard.html",find="Hariharan")'''
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
